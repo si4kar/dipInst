4bb627ed145c6bb2d5ade73ba4f410a115034b50
Sergii.Sichkar <si4ka@ukr.net>
2018-06-25 15:54:28 +0300

    fixed Auth controller

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -12,7 +12,7 @@ use frontend\models\PasswordResetRequestForm;
 use frontend\models\ResetPasswordForm;
 use frontend\models\SignupForm;
 use frontend\models\ContactForm;
-
+use frontend\components\AuthHandler;
 /**
  * Site controller
  */
@@ -62,7 +62,16 @@ class SiteController extends Controller
                 'class' => 'yii\captcha\CaptchaAction',
                 'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
             ],
+            'auth' => [
+                'class' => 'yii\authclient\AuthAction',
+                'successCallback' => [$this, 'onAuthSuccess'],
+            ],
         ];
+    }
+
+    public function onAuthSuccess($client)
+    {
+        (new AuthHandler($client))->handle();
     }
 
     /**
